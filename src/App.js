b996291f5b4f9924cc55523f19b2764337efd31b
BlackDeViL1906 <94516956+BlackDeViL1906@users.noreply.github.com>
2022-11-26 15:51:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import { React, useState } from 'react';
+import Select from 'react-select';
+import './style.css';
+
+function Dropdown ()
+{
+    const colors = [
+        {
+            id: 1,
+            label: "lime",
+        },
+        {
+            id: 2,
+            label: "lavender",
+        },
+        {
+            id: 3,
+            label: "crimson",
+        },
+        {
+            id: 4,
+            label: "darkblue",
+        },
+        {
+            id: 5,
+            label: "teal",
+        },
+        {
+            id: 6,
+            label: "rebeccapurple",
+        },
+        {
+            id: 7,
+            label: "ghostwhite",
+        },
+        {
+            id: 8,
+            label: "aquamarine",
+        },
+        {
+            id: 9,
+            label: "aliceblue",
+        },
+    ]
+
+    const [color, setColor] = useState("");
+
+    const click = (event) => {
+        setColor(event.label);
+    }
+    return (
+        <div className='dd'>
+            <style>
+            {
+                `.button 
+                {
+                 background-color:` + color + ';'
+                }
+            </style>
+
+            <center>
+            <h1>Choose to change color</h1>
+            </center>
+            
+            <div className="hh">
+                <Select options={colors} onChange={click} className='select' placeholder="Select your req Color"></Select>
+            </div>
+            <div className="block">
+                
+                    <button className='button'></button>
+                    
+                
+            </div>
+        </div>
+
+    );
+}
+export default Dropdown;
